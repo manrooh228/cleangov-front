fbd6dc1b664da107213c630e5a240e1aca2959fd
manrooh228 <yadaunmaksat@gmail.com>
2024-10-28 19:34:21 +0500

    Login page design + router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate }  from 'react-router-dom';
+import Login from '../src/component/pages/Login.js'
 
 function App() {
   return (
-   <h1>Niggas</h1>
+   <Router>
+      <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>
+   </Router>
   );
 }
 
--- src/component/pages/Login.js
+++ src/component/pages/Login.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react';
+import "../css/Login.css";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-
     return (
-        <h1>Login</h1>
+        <div className="LoginPage">
+            <div className="wrapper">
+                <h2>LOGIN</h2>
+                <form action = "#">
+                    <div className = "input-box">
+                        <input type='text' placeholder='Enter your username' required />
+                    </div>
+                    <div className = "input-box">
+                        <input type='password' placeholder='Enter your password'  required />
+                    </div>
+                    <div className = "policy">
+                        <input type='checkbox' />
+                        <h3>I accept terms & conditions</h3>
+                    </div>
+                    <div className="input-box button">
+                        <input type="submit" value="LOGIN" />
+                    </div>
+                    <div className='text'>
+                        <h3>Dont have an account? <a>Register</a></h3>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 };
 
