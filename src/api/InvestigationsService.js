637123a2745ca9346fe460d26b2fb1fba9b84c60
manrooh228 <maksatsuper57915@gmail.com>
2024-12-08 15:22:14 +0500

    console Investigations

--- src/api/InvestigationsService.js
+++ src/api/InvestigationsService.js
@@ -5,6 +5,7 @@ const API_BASE_URL = "http://localhost:1306/investigations";
 export const getAvailableInvestigations = async () => {
     try {
         const response = await axios.get(`${API_BASE_URL}/all`);
+        console.log(response.data);
         return response.data;
     } catch (error) {
         console.error("Error fetching available investigations:", error);
@@ -17,6 +18,7 @@ export const getTasksWithProgress = async (userId) => {
         const response = await axios.get(`${API_BASE_URL}/progress`, {
             params: { userId },
         });
+        console.log(response.data);
         return response.data;
     } catch (error) {
         console.error("Error fetching tasks with progress:", error);
