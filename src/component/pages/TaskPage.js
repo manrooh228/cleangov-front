e38598c6e3b03c5036376a3d2655c83365307ab3
manrooh228 <maksatsuper57915@gmail.com>
2024-12-11 00:33:13 +0500

    TaskPage test v1.0

--- src/component/pages/TaskPage.js
+++ src/component/pages/TaskPage.js
@@ -38,25 +38,104 @@ const TaskPage = () => {
         <>
             <Header />
             <div className="tasks-page">
+                <div className="tasks-page-title">
                 <h1>Tasks for Investigation #{investigationId}</h1>
+                </div>
                 <div className="tasks-list">
                     {tasks.map((task, index) => (
                         task.taskType === 'video' ? (
-                            <div key={index}>
-                                <h3>Video Task: {task.name}</h3>
-                                <p>{task.description}</p>
-                             </div>
-                        ) :
-                        task.taskType === 'test' ? (
-                            <div key={index}>
-                                <h3>Test Task: {task.name}</h3>
-                                <p>{task.description}</p>
+                            // <div key={index}>
+                            //     <h3>Video Task: {task.name}</h3>
+                            //     <p>{task.description}</p>
+                            //  </div>
+
+                            <div className="video-main-panel">
+                                <div className="video-main-title">
+                                    <h2>Task One</h2>
+                                    <h2>{task.name}</h2>
+                                </div> 
+                                <div className="video-description-panel">
+                                    <div className="panel-title">
+                                        <h4>Theoretical video</h4>
+                                    </div>
+                                    <div className="panel-info">
+                                        <h4>{task.description}</h4>
+                                    </div>
+                                </div>
+                                <div className="video-completed-panel">
+                                    <div className="panel-title">
+                                        <h4>Completed?</h4>    
+                                    </div>
+                                    <div className="checkbox">
+                                    {task.progress === 100 ? (
+                                        <button className="completed?"><i className='bx bx-check'></i></button>
+                                    ) : (
+                                        <button className="completed?"><i className='bx bx-x'></i></button>
+                                    )}
+                                    </div>
+                                </div>
+                                {task.progress !== 100 && (
+                                    <button className="inv-start"><p>START</p></button>
+                                )}
                             </div>
                         ) :
+                        task.taskType === 'test' ? (
+                            <div className="video-main-panel">
+                                <div className="video-main-title">
+                                    <h2>Task Two</h2>
+                                    <h2>{task.name}</h2>
+                                </div> 
+                                <div className="video-description-panel">
+                                    <div className="panel-title">
+                                        <h4>A test for verification knowledge</h4>
+                                    </div>
+                                    <div className="panel-info">
+                                        <h4>{task.description}</h4>
+                                    </div>
+                                </div>
+                                <div className="video-completed-panel">
+                                    <div className="panel-title">
+                                        <h4>Completed?</h4>    
+                                    </div>
+                                    <div className="checkbox">
+                                    {task.progress === 100 ? (
+                                        <button className="completed?"><i className='bx bx-check'></i></button>
+                                    ) : (
+                                        <button className="completed?"><i className='bx bx-x'></i></button>
+                                    )}
+                                    </div>
+                                </div>
+                                <div className="result-panel">
+                                    <div className="panel-title">
+                                        <h4>Completed?</h4>    
+                                    </div>
+                                    <div className="result">
+                                        <h4>100/100</h4> {/* vremenno */}
+                                    </div>
+                                </div>
+                                {task.progress !== 100 && (
+                                    <button className="inv-start"><p>START</p></button>
+                                )}
+                            </div>
+
+                        ) :
                         task.taskType === 'reflexio' ? (
-                            <div key={index}>
-                                <h3>Reflection Task: {task.name}</h3>
-                                <p>{task.description}</p>
+                            <div className="video-main-panel">
+                                <div className="video-main-title">
+                                    <h2>Task three</h2>
+                                    <h2>{task.name}</h2>
+                                </div> 
+                                <div className="video-description-panel">
+                                    <div className="panel-title">
+                                        <h4>Reflections on this investigation</h4>
+                                    </div>
+                                    <div className="panel-info">
+                                        <h4>{task.description}</h4>
+                                    </div>
+                                </div>
+                                {task.progress !== 100 && (
+                                    <button className="inv-start"><p>START</p></button>
+                                )}
                             </div>
                         ) : (
                             <div></div>
