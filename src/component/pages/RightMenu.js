25356445ceb8267903b05fae620fd1b45d019076
manrooh228 <maksatsuper57915@gmail.com>
2024-12-12 23:04:35 +0500

    Fixes

--- src/component/pages/RightMenu.js
+++ src/component/pages/RightMenu.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/RightMenu.css"
 import { useTranslation } from "react-i18next";
 import { useUser } from "../../api/context/UserProfile";
+
 
 const RightMenu = () => {
     const { user } = useUser();
     const {t} = useTranslation();
     console.log(user);
+    
     return (
         <>
         <div className="right-menu-main">
@@ -32,5 +34,26 @@ const RightMenu = () => {
         </>
     )
 }
+
+
+const SettingsMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleSettings = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    return (
+      <div className="menu-item">
+        <button className="menu-button" onClick={toggleSettings}>
+          Настройки
+        </button>
+        <div className={`settings-content ${isOpen ? "open" : ""}`}>
+          <button className="submenu-button">Язык</button>
+          <button className="submenu-button">Сменить тему</button>
+        </div>
+      </div>
+    );
+  };
 
 export default RightMenu;
