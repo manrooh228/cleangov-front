2c82e5d72146ec2fb8003217640829d32570a37a
manrooh228 <maksatsuper57915@gmail.com>
2024-12-13 01:10:31 +0500

    Achievements

--- src/component/pages/Profile.js
+++ src/component/pages/Profile.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../css/Profile.css"
 import "../css/Achievement.css"
 import { useUser } from "../../api/context/UserProfile";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { API_BASE_URL } from "../../api/API_BASE_URL";
 
 const Profile = () => {
     const { user } = useUser();
     const [showAchievements, setShowAchievements] = useState(false);
-    const {t} = useTranslation();
+    const [achievements, setAchievements] = useState([]);
+    const { t } = useTranslation();
 
     const handleAchievemntClick = () => {
         setShowAchievements((prev) => !prev);
-    }
+    };
+
+    useEffect(() => {
+        // Запросить достижения пользователя с сервера
+        const fetchAchievements = async () => {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/achievements/${user.id}/achievements`);
+                setAchievements(response.data);
+            } catch (error) {
+                console.error("Error fetching achievements:", error);
+            }
+        };
+
+        if (user) {
+            fetchAchievements();
+        }
+    }, [user]);
+
+    // Проверка, получил ли пользователь конкретную ачивку
+    const hasAchievement = (achievementName) => {
+        return achievements.some((ach) => ach.name === achievementName && ach.achieved);
+    };
+
     return (
         <>
-        {/* {user ? ( */}
             <div className="profile-main-panel">
                 <div className="profile-user-panel">
                     <div className="profile-user-pic"><i className='bx bxs-user-circle'></i></div>
@@ -23,8 +46,6 @@ const Profile = () => {
                         <h2>{user.username}</h2>
                         <h3>{user.name} {user.surname}</h3>
                     </div>
-                    {/* <h4>user.username</h4>
-                    <h5>user.id</h5> */}
                 </div>
                 <div className="profile-button" onClick={handleAchievemntClick}>
                     <h2>{t("investigation-list.achivements")}</h2>
@@ -47,13 +68,27 @@ const Profile = () => {
                         <h1 onClick={handleAchievemntClick}>x</h1>
                     </div>
                     <div className="achieves-list">
-                        
+                        <div className="achivement-item">
+                            <img 
+                                src={require(`../img/achievement-1${hasAchievement("The young investigator") ? "-true" : ""}.png`)} 
+                                alt="The young investigator" 
+                                className="image" 
+                            />
+                            <h4>The young investigator</h4>
+                        </div>
+                        <div className="achivement-item">
+                            <img 
+                                src={require(`../img/achievement-2${hasAchievement("The young geek") ? "-true" : ""}.png`)} 
+                                alt="The young geek" 
+                                className="image" 
+                            />
+                            <h4>The young geek</h4>
+                        </div>
                     </div>
                 </div>
             )}
-            
         </>
-    )
-}
+    );
+};
 
 export default Profile;
